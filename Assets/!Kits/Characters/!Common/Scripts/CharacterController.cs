4e3d0cdeb3a307a852f8250dc4d9708685b7d1f6
John Cuellar <jhon.cuellar.1996@hotmail.com>
2026-06-10 03:33:47 -0500

    Finish_Class_9

--- Assets/!Kits/Characters/!Common/Scripts/CharacterController.cs
+++ Assets/!Kits/Characters/!Common/Scripts/CharacterController.cs
@@ -5,9 +5,12 @@ public class CharacterController : MonoBehaviour
     [SerializeField] float movementSpeed = 3.0f;
 
     Rigidbody2D rb2D;
+    Animator animator;
+
     private void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();    
     }
 
     // Update is called once per frame
@@ -20,5 +23,7 @@ public class CharacterController : MonoBehaviour
     public void SetRawMove (Vector2 rawMove)
     {
         this.rawMove = rawMove;
+        animator.SetFloat("HorizontalVelocity", rawMove.x);
+        animator.SetFloat("VerticalVelocity", rawMove.y);
     }
 }
---
+++ Assets/Kits/LifeSystem/Scripts/LifeCanvas.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeCanvas : MonoBehaviour
+{
+    [SerializeField] Life life;
+    [SerializeField] Image mask;
+
+    private void Awake()
+    {
+        life.onLifeChanged.AddListener(onLifeChanged);
+    }
+
+    //private void OnEnable()
+    //{
+    //    life.onLifeChanged.AddListener(onLifeChanged);
+    //    //life.onLifeDepleted.AddListener(onLifeDepleted);
+    //}
+
+    //private void OnDisable()
+    //{
+    //    life.onLifeChanged.RemoveListener(onLifeChanged);
+    //    //life.onLifeDepleted.AddListener(onLifeDepleted);
+    //}
+    private void OnDestroy()
+    {
+        // Nos desuscribimos solo cuando el objeto es destruido por completo (con Destroy),
+        // no cuando simplemente se oculta temporalmente (SetActive(false)).
+        if (life != null)
+        {
+            life.onLifeChanged.RemoveListener(onLifeChanged);
+        }
+    }
+
+    private void onLifeChanged(float currentLife, float startLife)
+    {
+        mask.fillAmount = currentLife / startLife;
+    }
+
+    //private void onLifeDepleted(float startLife)
+    //{
+    //    throw new NotImplementedException();
+    //}
+}
